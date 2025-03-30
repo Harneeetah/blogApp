3d52d8c829c3316f8b36c1114838d9da036d3ee9
harneeetah <harneeetah@gmail.com>
2025-03-30 18:04:07 -0400

    adjustments

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 import express from "express";
-import bodyParser from "body-parser";
+import path from "path";
 import ejs from "ejs";
+import morgan from "morgan";
 import _  from "lodash";
-import mongoose from "mongoose";
 import dotenv from "dotenv";
 import Post from "./model/blogModel.js" ;
+// import connectDB from "./db/connectdb.js";
+dotenv.config({});
+
+
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -12,14 +16,18 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 const composeContent = "Fill the dialog boxes below to compose a post."
 const app = express();
 app.locals._ = _
-
+const __dirname = path.resolve();
+// connectDB()
+app.use(express.urlencoded({extended: true}));
+app.use(express.json())
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs');
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
-dotenv.config();
-
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4002;
 const MONGOURL = process.env.MONGO_URL; 
 
 app.get("/", (req, res) => {
@@ -40,7 +48,7 @@ app.get("/compose", (req, res) => {
   res.render('compose', {composeContent: composeContent})
 })
 
-app.post("/compose", (req, res) => {
+app.post("/compose", async (req, res) => {
   //{postTitle: req.body.postTitle, postBody: req.body.postBody}
   const post = new Post({
     postTitle: req.body.postTitle,
@@ -61,6 +69,6 @@ app.get("/posts/:postId", (req, res) => {
 })
 
 
-app.listen(process.env.PORT || 4000, function() {
-  console.log("Server started on port 4000");
+app.listen(process.env.PORT || 4002, function() {
+  console.log("Server started on port 4002");
 });
---
+++ db/connectdb.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config();
+
+const connectDB = async () => {
+try {
+// mongoose.Promise = global.Promise
+const conn = await mongoose.connect(process.env.MONGO_URI, {
+// useNewUrlParser: true,
+// useUnifiedTopology: true,
+// useFindAndModify: false,
+// useCreateIndex: true,
+})
+// console.log(conn)
+console.log(`💻 ${conn.connection.host} CONNECTED to the DB successfully`)
+} catch (err) {
+console.log(`Error ${err.message}`)
+process.exit(1)
+
+}
+}
+
+export default connectDB
